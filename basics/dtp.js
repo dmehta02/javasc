fdb993a42551924d153eb4208227ae6f9f121fac
dmehta02 <158476655+dmehta02@users.noreply.github.com>
2025-02-01 18:31:38 +0000

    javasc

---
+++ basics/dtp.js
@@ -0,0 +1,7 @@
+//"use strict" //new ver
+//alert (hello)
+//number, bigint,boolean,null(stanalone),undefined,symbol(unique),object
+
+
+
+console.log(typeof "hi")
---
+++ basics/variables.js
@@ -0,0 +1,14 @@
+const accountID = 12109800
+let accountMail = "tokyohouldb02@gmail.com"
+var accPass = "Somya"
+accCity = "Meerut"
+let accState
+
+//accountID = 1212
+accountMail ="dmm@gmsil.com"
+accPass= "Som"
+accCity="Delhi"
+
+
+console.log(accountID);
+console.table([accountMail,accPass,accCity,accState])
